000b3803c92d01604e9611bbc2917818131f602f
Alex <cigan@ua.fm>
2020-12-24 14:04:09 +0200

    Use children in routes of path to display two components one url
    <router-view></router-view> in a parent component is a place for display children of path

--- src/components/teams/TeamsList.vue
+++ src/components/teams/TeamsList.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- router-view - display children of TeamList from main.js  -->
+  <router-view></router-view>
   <ul>
     <teams-item
       v-for="team in teams"
--- src/main.js
+++ src/main.js
@@ -10,13 +10,18 @@ import NotFound from './components/nav/NotFound';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        // {path: '/', redirect: '/teams'}, // change url
-        {path: '/teams', component: TeamsList, alias: '/'}, // alias doesn't change url and display TeamsList
+        {path: '/', redirect: '/teams'}, // change url
+        // {path: '/teams', component: TeamsList, alias: '/'}, // alias doesn't change url and display TeamsList
         // {path: '/teams', component: TeamsList}, // url: our-domain/teams, content: TeamList
+        {
+            path: '/teams', component: TeamsList, children: [
+                {path: '/teams/:teamId', component: TeamMembers, props: true}
+            ]
+        },
         {path: '/users', component: UsersList},
         // dynamic adding address. It should be at the end of the list
         // props: true - dynamic component should added as props, TeamMembers.vue, props: teamId
-        {path: '/teams/:teamId', component: TeamMembers, props: true},
+        // {path: '/teams/:teamId', component: TeamMembers, props: true}, // remove to TeamList as children
         // {path: '/:notFound(.*)', redirect: '/users'} // if url doesn't exist redirect
         {path: '/:notFound(.*)', component: NotFound} // if url doesn't exist
     ],
